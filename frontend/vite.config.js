0346048fbcf51de6935583442ec8cf3084b3d804
mcarbono3 <ing.mariocarbono@gmail.com>
2025-06-10 09:07:36 -0500

    vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wind-analysis/',
+  base: '/wind-analysis/frontend/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
