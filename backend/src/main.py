333bc31266dcb32b0c90b0dd8f32cb1a6da0f4a0
mcarbono3 <ing.mariocarbono@gmail.com>
2025-06-17 14:39:37 -0500

    main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -8,6 +8,8 @@ from flask_cors import CORS
 from src.models.user import db
 from src.routes.user import user_bp
 from src.routes.climate import climate_bp
+from src.routes.era5 import era5_bp
+from src.routes.analysis import analysis_bp
 
 app = Flask(__name__, static_folder=os.path.join(os.path.dirname(__file__), 'static'))
 app.config['SECRET_KEY'] = 'asdf#FGSgvasgf$5$WGT'
